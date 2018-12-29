abfdb09ec30079a84cae7072cf6e3b3e9b05fd11
xv88133536@163.com <xv88133536@163.com>
2018-12-29 13:37:18 +0800

    spring boot  新增打印日志配置

--- mm-web/src/main/java/com/wasu/springboot/integration/controller/HelloController.java
+++ mm-web/src/main/java/com/wasu/springboot/integration/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.wasu.springboot.integration.controller;
 
 import com.wasu.springboot.integration.entity.system.UserInfo;
 import com.wasu.springboot.integration.utils.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(HelloController.class);
 
     @RequestMapping("/hello")
     public String say(ModelMap modelMap){
@@ -20,10 +24,12 @@ public class HelloController {
     @RequestMapping("/goodbye")
     @ResponseBody
     public String goodbye(){
+        LOGGER.info("enter goodbye ---");
         UserInfo result=new UserInfo();
         result.setName(StringUtils.omit("徐健",1));
         result.setAge(100);
         result.setPasswd("123456");
+        LOGGER.info("---leave goodbye !");
         return result.toString();
     }
 }
