d82d41a6b5de60356944f8f4501e97655be8f103
xv88133536@163.com <xv88133536@163.com>
2019-02-18 17:16:22 +0800

    spring boot  新增模式类

---
+++ mm-core/src/main/java/com/wasu/springboot/integration/utils/PatternUtils.java
@@ -0,0 +1,20 @@
+package com.wasu.springboot.integration.utils;
+
+public class PatternUtils {
+
+    /**
+     * 18位身份证号
+     */
+    public static final String IDENTIFY_PATTERN="^(\\d{6})(\\d{4})(\\d{2})(\\d{2})(\\d{3})([0-9]|X)$";
+
+    /**
+     * 年月日格式
+     */
+    public static final String YEAR_MONTH_DAY_PATTERN="([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8])))";
+
+    /**
+     * ip地址格式
+     */
+    public static final String IP_PATTERN=" (2(5[0-5]{1}|[0-4]\\d{1})|[0-1]?\\d{1,2})(\\.(2(5[0-5]{1}|[0-4]\\d{1})|[0-1]?\\d{1,2})){3}";
+
+}
