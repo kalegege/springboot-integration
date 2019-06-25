1bca7678014ce11e1adc53b509d30e26d37a69cc
xv88133536@163.com <xv88133536@163.com>
2019-06-25 15:49:57 +0800

    修改配置文件

--- mm-config/src/main/java/com/wasu/springboot/integration/ConfigApplication.java
+++ mm-config/src/main/java/com/wasu/springboot/integration/ConfigApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
-public class SpringbootConfigApplication {
+public class ConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootConfigApplication.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
 
 }
