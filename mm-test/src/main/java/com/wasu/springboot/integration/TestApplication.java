f684aa345b101fc7a4d4e965e32caf87b887be8d
xv88133536@163.com <xv88133536@163.com>
2019-07-12 17:37:51 +0800

    新增测试微服务

---
+++ mm-test/src/main/java/com/wasu/springboot/integration/TestApplication.java
@@ -0,0 +1,16 @@
+package com.wasu.springboot.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
+
